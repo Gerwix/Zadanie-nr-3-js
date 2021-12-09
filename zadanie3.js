d620a39123d7c23421b572c58d48bd3f08379191
Gerwix <mateuszgerwazik@gmail.com>
2021-12-09 18:33:19 +0100

    Initial commit

---
+++ zadanie3.js
@@ -0,0 +1,2 @@
+var value1 = 'A B R A K A D A B R A\n A B R A K A D A B R\n  A B R A K A D A B\n   A B R A K A D A\n    A B R A K A D\n     A B R A K A\n      A B R A K\n       A B R A\n        A B R\n         A B\n          A';
+console.log(value1);
